2cfe434e3738dc65b98acbad1f5df826f98fcff5
Mimoh123 <mppalikhe@gmail.com>
2025-05-27 22:21:52 +0545

    fixed the file structure and used vanilla html and js

--- Assignment/Assignment 2/todo/index.js
+++ Assignment/Assignment 2/todo/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5500'],
     credentials: true,
   })
 );
