df0cc7affd02a6c0a34f57199608851e420bbe8e
Mimoh123 <mppalikhe@gmail.com>
2025-05-28 07:09:36 +0545

    changed the folder name to project1

---
+++ Frontend-vanilla/script.js
@@ -0,0 +1,12 @@
+console.log('hello');
+
+const formval = document.querySelector('#aadarsh');
+
+const val = formval.value;
+
+console.log(val);
+function greet(e) {
+  e.preventDefault();
+  console.log('Submitted');
+}
+function treat() {}
